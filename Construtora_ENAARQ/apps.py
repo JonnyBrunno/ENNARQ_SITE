6064cf8f3ac63a00689efe1be14b034283a69310
JonnyBrunno <teste>
2026-02-28 12:31:58 -0300

    Minha primeira versão do trabalho

---
+++ Construtora_ENAARQ/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConstrutoraEnaarqConfig(AppConfig):
+    name = 'Construtora_ENAARQ'
---
+++ Construtora_ENAARQ/models.py
@@ -0,0 +1,7 @@
+#from django.db import models
+#class Notas(models.Model):
+#nome_aluno = models.Charfeld(max_length=250)
+#    nota =models.DecimaField(max_digits=5, decimal_places=2)
+#
+#    def __str__(self):
+#      return self.nome_aluno
